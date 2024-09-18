a2b1cfc45d76011a91c6cba372caef1061208b31
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-09-18 15:24:29 +0200

    [UPD] Created a first class

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
+class Prodotto{
+    public $nome;
+    public $prezzo;
+    public $categoria;
+
+    function __construct($_nome, $_prezzo) {
+        $this->nome = $_nome;
+        $this->prezzo = $_prezzo;
+    }
+}
+
 ?>
